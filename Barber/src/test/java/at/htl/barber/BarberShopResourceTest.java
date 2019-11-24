c6812896feefe42c8bec5d4581656983b1cca884
nico <nico.vasquez@gmx.at>
2019-11-24 14:09:16 +0100

    added REST endpoint tests

---
+++ Barber/src/test/java/at/htl/barber/BarberShopResourceTest.java
@@ -0,0 +1,41 @@
+package at.htl.barber;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+public class BarberShopResourceTest {
+
+    @Test
+    public void test01_getAllBarbershops(){
+        given()
+            .when().get("/barbershop")
+            .then()
+                .statusCode(200)
+                .body("$.size()", is(3),
+                        "[0].name", is("Klipp"),
+                        "[1].name", is("DM"),
+                        "[2].name", is("Schnittzone"));
+    }
+
+    @Test
+    public void test02_getBarbershopByName(){
+        given()
+            .when().get("/barbershop/Klipp")
+            .then()
+                .statusCode(200)
+                .body("name", is("Klipp"));
+    }
+
+    @Test
+    public void test03_noBarbershopFound(){
+        given()
+            .when().get("/barbershop/Shop123")
+            .then()
+                .statusCode(404);
+    }
+
+}
---
+++ Barber/src/test/java/at/htl/barber/EmployeeResourceTest.java
@@ -0,0 +1,31 @@
+package at.htl.barber;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class EmployeeResourceTest {
+
+    @Test
+    public void test01_getAllEmployees(){
+        given()
+            .when().get("/employee")
+            .then()
+                .statusCode(200)
+                .body("$.size()", is(2),
+                        "[0].name", is("Sophie"),
+                        "[1].name", is("Dianna"));
+    }
+
+    @Test
+    public void test02_getEmployeeByName(){
+        given()
+            .when().get("/employee/Sophie")
+            .then()
+            .statusCode(200)
+            .body("name", is("Sophie"));
+    }
+}
