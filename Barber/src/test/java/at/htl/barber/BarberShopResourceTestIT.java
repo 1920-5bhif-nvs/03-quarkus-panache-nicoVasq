93c906a808c5ea723056517af0fa6e051539d38d
nicoVasq <nico.vasquez@gmx.at>
2019-12-02 17:56:01 +0100

    Added POST and DELETE test

--- Barber/src/test/java/at/htl/barber/BarberShopResourceTestIT.java
+++ Barber/src/test/java/at/htl/barber/BarberShopResourceTestIT.java
@@ -1,12 +1,18 @@
 package at.htl.barber;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class BarberShopResourceTestIT {
 
     @Test
@@ -37,5 +43,29 @@ public class BarberShopResourceTestIT {
             .then()
                 .statusCode(404);
     }
+
+    @Test
+    public void test_04_postBarbershop(){
+        Response response =
+                given()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Figaro\"}")
+                .post("/barbershop")
+                .then()
+                    .statusCode(200)
+                    .extract().response();
+
+        assertThat(response.jsonPath().getString("name")).isEqualTo("Figaro");
+    }
+
+    @Test
+    public void test_05_deleteBarberShop(){
+        given()
+            .when().delete("/barbershop/DM")
+            .then()
+                .statusCode(200);
+    }
+
+
 
 }
