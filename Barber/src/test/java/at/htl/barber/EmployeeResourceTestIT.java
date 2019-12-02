6100e5ee4da84a1c44bf38654621e3b8882160cc
nicoVasq <nico.vasquez@gmx.at>
2019-12-02 17:55:12 +0100

    Added CRUD tests

--- Barber/src/test/java/at/htl/barber/EmployeeResourceTestIT.java
+++ Barber/src/test/java/at/htl/barber/EmployeeResourceTestIT.java
@@ -1,12 +1,18 @@
 package at.htl.barber;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class EmployeeResourceTestIT {
 
     @Test
@@ -28,4 +34,43 @@ public class EmployeeResourceTestIT {
             .statusCode(200)
             .body("name", is("Sophie"));
     }
+
+    @Test
+    public void test03_postEmployee(){
+         Response response =
+                 given()
+                 .contentType(ContentType.JSON)
+                 .body("{\"name\":\"Phil\",\"barberShop\":{\"name\":\"Klipp\"},\"salary\":1300.0}")
+                 .post("/employee")
+                 .then()
+                    .statusCode(200)
+                    .extract().response();
+
+         assertThat(response.jsonPath().getString("name")).isEqualTo("Phil");
+    }
+
+    @Test
+    public void test_04_putEmployee(){
+        //Update the employees salary
+        Response response =
+                given()
+                .contentType(ContentType.JSON)
+                .body("{\"name\":\"Phil\",\"barberShop\":{\"name\":\"Klipp\"},\"salary\":1500.0}")
+                .post("/employee")
+                .then()
+                    .statusCode(200)
+                    .extract().response();
+
+        assertThat(response.jsonPath().getDouble("salary")).isEqualTo(1500.0);
+    }
+
+    @Test
+    public void test_05_deleteEmployee(){
+        given()
+            .when().delete("/employee/Phil")
+            .then()
+                    .statusCode(200);
+    }
+
+
 }
